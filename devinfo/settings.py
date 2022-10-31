ec9873438ccefcbc7ee0b0d5bde8b15d0adf6e8f
mahbubrahman1 <mahbuburrahman3@yahoo.com>
2022-10-31 15:55:49 +0600

    styling the page

--- devinfo/settings.py
+++ devinfo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_*8iu)7jl)i%(#@f)4_$d9f9w%pqd*2uj-)5*ylf_b)cb7*%rp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
--- projects/forms.py
+++ projects/forms.py
@@ -7,3 +7,15 @@ class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
         fields = ['title', 'thumbnail', 'description', 'live_site', 'source_code', 'tags']
+        widgets = {
+            'tags':forms.CheckboxSelectMultiple(),
+        }
+    
+    # implement style in input form 
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+
+        self.fields['title'].widget.attrs.update({'class': 'input'})
+        self.fields['description'].widget.attrs.update({'class': 'input'})
+        self.fields['live_site'].widget.attrs.update({'class': 'input'})
+        self.fields['source_code'].widget.attrs.update({'class': 'input'})
