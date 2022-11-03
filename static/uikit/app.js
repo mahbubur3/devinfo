4485e3984dbc89944ed8fc063f4dc4f32a1bad0d
mahbubrahman1 <mahbuburrahman3@yahoo.com>
2022-11-03 18:19:40 +0600

    updated via some file

--- static/uikit/app.js
+++ static/uikit/app.js
@@ -1,14 +1,10 @@
-// Invoke Functions Call on Document Loaded
-document.addEventListener('DOMContentLoaded', function () {
-  hljs.highlightAll();
-});
-
-
-let alertWrapper = document.querySelector('.alert')
-let alertClose = document.querySelector('.alert__close')
+let alertWrapper = document.querySelectorAll('.alert');
+let alertClose = document.querySelectorAll('.alert__close');
 
 if (alertWrapper) {
-  alertClose.addEventListener('click', () =>
-    alertWrapper.style.display = 'none'
-  )
-}
+  for (let i = 0; i < alertClose.length; i++) {
+    alertClose[i].addEventListener('click', () => {
+      alertWrapper[i].style.display = 'none'
+    });
+  }
+} 
