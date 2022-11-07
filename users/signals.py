8f4cb312916a756e900082910ef1817635dd8b9f
mahbubrahman1 <mahbuburrahman3@yahoo.com>
2022-11-07 11:38:00 +0600

    added send mail funtionality

--- users/signals.py
+++ users/signals.py
@@ -1,6 +1,8 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from django.conf import settings
 
 from .models import Profile
 
@@ -8,7 +10,24 @@ from .models import Profile
 def create_profile(sender, instance, created, **kwargs):
     if created:
         user = instance
-        profile = Profile.objects.create(user=user, username=user.username, email=user.email, name=user.first_name)
+        profile = Profile.objects.create(
+            user=user,
+            username=user.username,
+            email=user.email,
+            name=user.first_name
+        )
+
+        subject = 'Welcome to DevInfo'
+        message = 'We are happy to join with us.'
+
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently=False
+
+        )
 
 
 def edit_user(sender, instance, created, **kwargs):
